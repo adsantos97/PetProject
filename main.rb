3b6d43d33c2989e7d6647604a78fe087ca9f408a
adsantos97 <asantos@cs.loyola.edu>
2018-03-21 03:22:41 -0400

    students stuff

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 =begin
-Arizza Santos
-CS451 Pet Program
-Solution
+Name:
+Why did you choose this pet language?
+
 =end
+
 require './store'
 
 def usage
@@ -15,40 +16,15 @@ def usage
 end
 
 def main
-  continue = true
   store = Store.new
 
   puts "Personal Movie Store"
   puts "What would you like to do?"
   usage
 
-  while continue
-    choice = gets.chomp.downcase
-    case choice
-    when "add"
-      store.add_movie
-      puts "----------"
-    when "update"
-      store.update_movie
-      puts "----------"
-    when "display"
-      store.display_movie
-      puts "----------"
-    when "remove"
-      store.remove_movie
-      puts "----------"
-    when "four"
-      mine = Proc.new { puts " is MINE!" }
-      store.four_rating_movie mine
-      puts "----------"
-    when "quit"
-      puts "Aww...bye bye!"
-      continue = false
-    else
-      puts "Sorry, I don't understand you."
-      usage
-    end
-  end   
+  # handle program operations here
+  # with the help of using the Store class
+
 end
 
 main
--- store.rb
+++ store.rb
@@ -1,83 +1,66 @@
-=begin
-Arizza Santos
-CS451 Pet Program
-Solution
-=end
+# Name: 
+
+# Multi-line comments in the methods are the exact puts statements I used
 
 class Store
   attr_reader :name
   attr_accessor :rating, :price
 
   def initialize
-    @movies = Hash.new
+    @movies = 
   end
 
   def add_movie
-    puts "What movie do you want to add?"
-    title = gets.chomp
-    if @movies[title.to_sym].nil?
+
+=begin
+
+      puts "What movie do you want to add?"
       puts "What is the rating? (Type a number 0 to 4)"
-      rating = gets.chomp
       puts "What is price do you want to sell it at? (Type an integer)"
-      price = gets.chomp
-      @movies[title.to_sym] = [rating.to_i, price.to_i]
       puts "#{title} has been added with a rating of #{rating} and with a price of #{price}."
-    else
+
       puts "You already have that movie!"
-      puts "Its rating is #{@movies[title.to_sym][0]}."
-      puts "Its price is #{@movies[title.to_sym][1]}."
-    end
+      puts "Its rating is ."
+      puts "Its price is ."
+=end
+
   end
 
   def update_movie
+
+=begin
     puts "What movie do you want to update?"
-    title = gets.chomp
-    if @movies[title.to_sym].nil?
-      puts "Movie not found!"
-    else
-      puts "Did you want to change the rating or price?"
-      change = gets.chomp.downcase
-      case change
-      when "rating"
+    puts "Movie not found!"
+    puts "Did you want to change the rating or price?"
         puts "What's the new rating? (Type a number 0 to 4)"
-        rating = gets.chomp
-        @movies[title.to_sym][0] = rating.to_i
         puts "#{title} has been updated with the new rating of #{rating}."
-      when "price"
         puts "What's the new price? (Type an integer)"
-        price = gets.chomp
-        @movies[title.to_sym][1] = price.to_i
         puts "#{title} has been updated with the new price of #{price}."
-      else
-        puts "Sorry, that is an invalid choice."
-      end
-    end
+    
+        puts "Sorry, that is an invalid choice."     
+=end
+
   end
 
   def display_movie
-    @movies.each do |title, data|
-      puts "#{title}-> rating: #{data[0]} | price: #{data[1]}"
-    end
+    
   end
 
   def remove_movie
+
+=begin
     puts "What movie do you want to remove?"
-    title = gets.chomp
-    if @movies[title.to_sym].nil?
       puts "Movie not found!"
-    else
-      @movies.delete(title.to_sym)
       puts "#{title} has been removed."
-    end
+=end
+
   end
 
   def four_rating_movie some_proc
-    @movies.each do |title, data|
       if data[0] == 4
         print "#{title}"
         some_proc.call
       end      
-    end
   end
 
 end
