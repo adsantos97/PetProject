2e9f9c570d04d306d9433166a3fa530002f70a6e
adsantos97 <asantos@cs.loyola.edu>
2018-03-21 02:48:52 -0400

    done?

--- solution/asn8.rb
+++ solution/asn8.rb
@@ -8,7 +8,7 @@ require './store'
 def usage
   puts "-- Type 'add' to add a movie."
   puts "-- Type 'update' to update a movie."
-  puts "-- Type 'display' to display all movie."
+  puts "-- Type 'display' to display all movies."
   puts "-- Type 'remove' to remove a movie."
   puts "-- Type 'four' for a surprise."
   puts "-- Type 'quit' to quit."
